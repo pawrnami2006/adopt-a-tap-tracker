89b6251774faf06703e10c7c7e70c8fea73f34bf
Merin Joy <jmerin2005@gmail.com>
2026-09-11 18:26:34 +0530

    capacitor init

---
+++ android/app/src/main/java/com/adoptatap/tracker/MainActivity.java
@@ -0,0 +1,5 @@
+package com.adoptatap.tracker;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.adoptatap.tracker',
+  appName: 'adopt-a-tap-tracker',
+  webDir: 'dist'
+};
+
+export default config;
